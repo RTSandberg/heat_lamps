ab7a2b98d9908ef0b612ade045c06ae0b378278e
RTSandberg <ryansand@umich.edu>
2020-03-31 23:51:42 -0400

    starting a debug of tracers on Great Lakes

--- heat_lamps/tracers.py
+++ heat_lamps/tracers.py
@@ -32,10 +32,10 @@ def add_tracers(self, xs_tracers, vs_tracers, dt):
     self.vs_new_tracers = 1. * vs_tracers
     self.vs_old_tracers = 1. * vs_tracers
 
-    Es_tracers = self.calc_E(self.xs_tracers, self.x0s,self.weights, self.L, self.delta)
+    self.Es_tracers = self.calc_E(self.xs_tracers, self.x0s,self.weights, self.L, self.delta)
     
-    self.vs_old_tracers -= .5 * dt * self.qm * Es_tracers
-    self.vs_new_tracers += .5 * dt * self.qm * Es_tracers
+    self.vs_old_tracers -= .5 * dt * self.qm * self.Es_tracers
+    self.vs_new_tracers += .5 * dt * self.qm * self.Es_tracers
 
 
 def update(self, dt):
@@ -51,8 +51,10 @@ def update(self, dt):
     self.xs_tracers += dt * self.vs_new_tracers
     self.xs_tracers = np.mod(self.xs_tracers, self.L)
     # self.Es = calc_E_atan(self.xs, self.xs, self.weights, self.L, delta)
+    self.Es_tracers = self.calc_E(self.xs_tracers, self.xs, self.weights, self.L,self.delta)
+    # print(Es_tracers)
     self.vs_old_tracers = 1.*self.vs_new_tracers
-    self.vs_new_tracers += dt * self.qm * self.calc_E(self.xs_tracers, self.xs, self.weights, self.L,self.delta)
+    self.vs_new_tracers += dt * self.qm * self.Es_tracers
 
 def diag_dump_leapfrog(self,iter_num):
     """
@@ -64,3 +66,4 @@ def diag_dump_leapfrog(self,iter_num):
     """
     np.ndarray.tofile(self.xs_tracers, self.output_dir + 'xs/xs_tracers_%i'%iter_num)
     np.ndarray.tofile(.5 * (self.vs_old_tracers + self.vs_new_tracers), self.output_dir + 'vs/vs_tracers_%i'%iter_num)
+    np.ndarray.tofile(self.Es_tracers, self.output_dir + 'Es/Es_tracers_%i'%iter_num)
